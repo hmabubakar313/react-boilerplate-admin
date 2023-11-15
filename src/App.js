4aeab599c79adaf82b6fa4853a4ae4d36c0f1a45
hmabubakar313 <refocuse313@gmail.com>
2023-11-15 16:54:27 +0500

    Login Contact us and signup form created and validation working fine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/navbar/Navbar'
-import About from './components/navbar/About';
-import Contact from './components/forms/Form';
-import Footer from './components/footer/Footer';
-import Home from './components/navbar/Home';
-
-
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import About from "./components/navbar/About";
+import Footer from "./components/footer/Footer";
+import Home from "./components/navbar/Home";
+import ContactUs from "./components/forms/ContactUs";
+import LoginPage from "./components/forms/LoginForm";
+import SignupPage from "./components/forms/SignUp";
 
 
 
@@ -19,8 +18,10 @@ const App = () => {
         <div className="container-fluid  m-0 p-0">
           <Routes>
             <Route path="/about" element={<About />} />
-            <Route path="/contact_us" element={<Contact />} />
+            <Route path="/contact_us" element={<ContactUs />} />
+            <Route path="/login" element={<LoginPage />} />
             <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignupPage />} />
           </Routes>
         </div>
         <Footer />
